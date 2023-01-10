ee36e05d8cc6be7efd364602285c666d1e646c3a
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-09 22:02:03 -0500

    Update index.js
    added 2 corinthians image

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         </h1>
         <div className={styles.hero}>
        <Image 
-                                src="/2corinthians.jpg"
+                                src="/2corinthians.jfif"
                                 alt="Jesus teaching"
                                 height={900}
                                 width={2000}
