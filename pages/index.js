346839345f1c44011ba91632fd0473cfe964b907
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-06-27 20:54:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -310,36 +310,11 @@ Final Greetings
        
 
         </div>
- <div className={styles.image}>
-       <Image 
-                                src="/workshop.png"
-                                alt="Jesus teaching"
-                                height={900}
-                                width={2000}
-                                priority
-                              />
-            </div>
 
-</div>
-        <p className={styles.description}> 
-        <code className={styles.publisher}>
-        Coded, Published & Deployed by His servant: Jason Daniel Nutt 
-        </code>
-       </p>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+ </main>
+                                  
+                             
     </div>
   )
 }
